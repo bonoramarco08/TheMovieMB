9d7d1f14aa0e403c40e2ba89e4ab1f303bb54525
bonny99 <bonoramarco08@gmail.com>
2020-04-13 15:54:56 +0200

    MovieTableHelper fatta

--- app/src/main/java/com/example/themoviemb/data/MovieTableHelper.java
+++ app/src/main/java/com/example/themoviemb/data/MovieTableHelper.java
@@ -1,4 +1,20 @@
 package com.example.themoviemb.data;
 
-public class MovieTableHelper {
+import android.provider.BaseColumns;
+
+public class MovieTableHelper implements BaseColumns {
+    public static final String TABLE_NAME = "movie";
+    public static final String TITLE = "title";
+    public static final String DESCRIPTION = "description";
+    public static final String COVER_PHOTO = "coverPhoto";
+    public static final String DESCRIPTION_PHOTO = "descriptionPhoto";
+    public static final String IS_FAVORITE = "isFavorite";
+
+    public static final String CREATE = "CREATE TABLE " + TABLE_NAME + " ( " +
+            _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            TITLE + " TEXT, " +
+            DESCRIPTION + " TEXT, " +
+            COVER_PHOTO + " TEXT, " +
+            DESCRIPTION_PHOTO + " TEXT, " +
+            IS_FAVORITE + " INTEGER ) ;";
 }
