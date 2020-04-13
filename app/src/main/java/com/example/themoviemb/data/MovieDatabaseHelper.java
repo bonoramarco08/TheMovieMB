7e690f294fa52db529dd476490ade0125b10673c
marcoBrotto99 <47603662+marcobrotto99@users.noreply.github.com>
2020-04-13 15:53:16 +0200

    Movie Database

---
+++ app/src/main/java/com/example/themoviemb/data/MovieDatabaseHelper.java
@@ -0,0 +1,24 @@
+package com.example.themoviemb.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MovieDatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "movies.db";
+
+    public MovieDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(MovieTableHelper.CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+}
