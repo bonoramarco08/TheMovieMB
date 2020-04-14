9c4ee94dbbff3b743a424a2ad9253a6c8f5dc233
marcoBrotto99 <47603662+marcobrotto99@users.noreply.github.com>
2020-04-14 12:07:02 +0200

    Sistemato Cell layout con immagine e aggiunta di string value per contentDescription

--- app/src/main/java/com/example/themoviemb/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/example/themoviemb/ui/favorite/FavoriteFragment.java
@@ -24,15 +24,6 @@ public class FavoriteFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(FavoriteViewModel.class);
         View root = inflater.inflate(R.layout.fragment_favorite, container, false);
-        /*
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        */
         return root;
     }
 }
