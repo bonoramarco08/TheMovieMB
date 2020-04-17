1575f90e8468745b29a9a59edd5e43fe00a9707a
bonny08 <bonoramarco08@gmail.com>
2020-04-17 15:41:03 +0200

    Layout Base activity description

--- app/src/main/java/com/example/themoviemb/activities/DescriptionActivity.java
+++ app/src/main/java/com/example/themoviemb/activities/DescriptionActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.themoviemb.R;
 import com.example.themoviemb.data.MovieProvider;
@@ -12,6 +14,8 @@ import com.example.themoviemb.data.MovieProvider;
 public class DescriptionActivity extends AppCompatActivity {
 
     private static int idMovie=-1;
+    ImageView descriptionImage;
+    TextView title, description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,7 @@ public class DescriptionActivity extends AppCompatActivity {
         if(idMovie!=-1){
             Cursor movie = getContentResolver().query(Uri.parse(MovieProvider.MOVIES_URI+"/"+idMovie),null,null,null,null);
             movie.moveToNext();
+            descriptionImage=findViewById(R.id.imgDescription);
 
         }
     }
